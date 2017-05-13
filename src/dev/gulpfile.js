963812ecc07761fa5ed50c81afb42406f49c6e06
Michael Kurowski <Byamarro@gmail.com>
2017-05-13 12:15:22 +0200

    [gulp][package] Added babel

--- src/dev/gulpfile.js
+++ src/dev/gulpfile.js
@@ -2,7 +2,8 @@
  
 const gulp = require('gulp')
 const sass = require('gulp-sass')
- 
+const babel = require('gulp-babel')
+
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
@@ -10,20 +11,28 @@ gulp.task('sass', function () {
 })
  
 gulp.task('copy', function () {
-  return gulp.src('./app/*')
+  return gulp.src('./app/**/*.html')
     .pipe(gulp.dest('../prod/'))
 })
 
 
-gulp.task('files:watch', function () {
-  gulp.watch('./app/*', ['copy'])
+gulp.task('html:watch', function () {
+  gulp.watch('./app/**/*.html', ['copy'])
 })
 
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass'])
 })
 
+gulp.task('babel', () => {
+    return gulp.src('./app/**/*.js')
+        .pipe(babel({presets: ['es2015']}))
+        .pipe(gulp.dest('../prod/'));
+})
+gulp.task('js:watch', () => {
+    gulp.watch('./app/**/*.js', ['babel'])
+})
 
 gulp.task('default', function () {
-  gulp.start('sass:watch', 'files:watch')
+  gulp.start('sass:watch', 'html:watch', 'js:watch')
 })
